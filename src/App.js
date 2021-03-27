bdacba3288c21d488698557120e7c12116cc298f
Mykiel-Horn <mykiel.d.horn24@gmail.com>
2021-03-27 17:22:12 -0500

    conflicts rebased

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import "./App.css";
-import navBar from "./components/navBar/navbar";
-import { AppBar } from "@material-ui/core";
-import { Link, Route, Switch, Redirect } from "react-router-dom";
+import './App.css';
+import navBar from './components/navBar/navbar';
+import { AppBar } from '@material-ui/core';
+import { Link, Route, Switch, Redirect } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <navBar />
-      <AppBar position="sticky" color="">
-        <nav className="navBar">
-          <h1>
-            <Link to="/">Home</Link>
-          </h1>
-          <h1>
-            <Link className="link" to="/favorites">
-              favorites
-            </Link>
-          </h1>
-          <h1>
-            <Link to="/vote"> Vote </Link>
-          </h1>
-          <h1>
-            <Link to="/artists">Artists</Link>
-          </h1>
-        </nav>
-      </AppBar>
-      <h1>hello world</h1>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<navBar />
+			<AppBar position='sticky' color=''>
+				<nav className='navBar'>
+					<h1>
+						<Link to='/'>Home</Link>
+					</h1>
+					<h1>
+						<Link className='link' to='/favorites'>
+							favorites
+						</Link>
+					</h1>
+					<h1>
+						<Link to='/vote'> Vote </Link>
+					</h1>
+					<h1>
+						<Link to='/artists'>Artists</Link>
+					</h1>
+				</nav>
+			</AppBar>
+			<h1>hello world</h1>
+		</div>
+	);
 }
 
 export default App;
