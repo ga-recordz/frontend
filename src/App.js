3a7dd1aee281be5a759f1bda419478b148429d66
Mykiel-Horn <mykiel.d.horn24@gmail.com>
2021-03-28 01:53:35 -0500

    working front to back connection

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import navBar from './components/navBar/navbar';
-import { AppBar } from '@material-ui/core';
-import { Link, Route, Switch, Redirect } from 'react-router-dom';
 
 function App() {
+	const [artists, setArtists] = useState([]);
+	useEffect(() => {
+		fetch('http://localhost:4000/artists')
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				setArtists(data);
+			});
+	}, []);
+
 	return (
 		<div className='App'>
-			<navBar />
-			<AppBar position='sticky' color=''>
-				<nav className='navBar'>
-					<h1>
-						<Link to='/'>Home</Link>
-					</h1>
-					<h1>
-						<Link className='link' to='/favorites'>
-							favorites
-						</Link>
-					</h1>
-					<h1>
-						<Link to='/vote'> Vote </Link>
-					</h1>
-					<h1>
-						<Link to='/artists'>Artists</Link>
-					</h1>
-				</nav>
-			</AppBar>
-			<h1>hello world</h1>
+			<nav>
+				<h3>Favorites</h3>
+				<h3>Home</h3>
+				<h3>SignIn</h3>
+			</nav>
+			<h1>Artists</h1>
+			{artists.map((artist) => {
+				return (
+					<div key={artist.artist} className='artistCard'>
+						<img src={artist.photo} alt={`${artist.artist}`} />
+						<h3>{artist.artist}</h3>
+						<div className='microphoneIcon'>
+							<img
+								src='https://img.icons8.com/emoji/48/000000/microphone-emoji.png'
+								alt='microphone to like'
+							/>
+							<p>1,245,365</p>
+						</div>
+					</div>
+				);
+			})}
 		</div>
 	);
 }
