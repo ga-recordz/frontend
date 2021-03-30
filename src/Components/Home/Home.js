3987840fb8b7e505b96aaa9a0e6bf6a2c2e2f78a
Mykiel-Horn <mykiel.d.horn24@gmail.com>
2021-03-30 14:00:50 -0500

    added top 5 component to the home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Top5 from '../Top5/Top5';
 
 const Home = () => {
-    return (
-        <div>
-            <h1>hello world</h1>
-        </div>
-    );
+	return (
+		<div>
+			<Top5 />
+		</div>
+	);
 };
 
 export default Home;
