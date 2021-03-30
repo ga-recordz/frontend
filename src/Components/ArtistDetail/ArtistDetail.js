56a7ce1317f46fc8e9ce0b5d02b7f2aa00aaeb59
Mykiel-Horn <mykiel.d.horn24@gmail.com>
2021-03-30 12:03:06 -0500

    delete this

--- src/Components/ArtistDetail/ArtistDetail.js
+++ src/Components/ArtistDetail/ArtistDetail.js
@@ -1,33 +1,37 @@
-import { useState, useEffect, useParmas } from "react";
-import "./ArtistDetail.css";
-import React from "react";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import './ArtistDetail.css';
 
 const ArtistDetail = ({ match }) => {
-  const [details, setDetails] = useState([]);
-  //   const URL = `http://localhost:4000/artists/${match.params.id}`;
+	const [details, setDetails] = useState([]);
+	let { artistName } = useParams();
+	//   const URL = `http://localhost:4000/artists/${match.params.id}`;
 
-  useEffect(() => {
-    const id = match.params.id;
-    fetch(`http://localhost:4000/artists/${id}`)
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-        console.log("hee");
-        setDetails(res);
-      });
-  }, [match.params.id]);
+	useEffect(() => {
+		const id = match.params.id;
+		console.log(id);
+		console.log(artistName);
+		fetch(`http://localhost:4000/artists/${id}`)
+			.then((res) => res.json())
+			.then((res) => {
+				console.log(res);
+				console.log('hee');
+				setDetails(res);
+			});
+	}, []);
 
-  if (!details) {
-    return <h1>...Loading</h1>;
-  }
+	if (!details) {
+		return <h1>...Loading</h1>;
+	}
 
-  return (
-    <div>
-      {/* <h1>hello world</h1> */}
-      <h1>{details.artist}</h1>
-      <h1>{details.bio}</h1>
-    </div>
-  );
+	return (
+		<div>
+			{/* <h1>hello world</h1> */}
+			<h1>{details.artist}</h1>
+			<h1>{details.bio}</h1>
+		</div>
+	);
 };
 
 export default ArtistDetail;
