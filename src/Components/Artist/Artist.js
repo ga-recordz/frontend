ab0cd58fb2167ab61f2e4cde356b6817e5332578
Mykiel-Horn <mykiel.d.horn24@gmail.com>
2021-03-30 11:07:12 -0500

    added random artist feature to the artist page

--- src/Components/Artist/Artist.js
+++ src/Components/Artist/Artist.js
@@ -1,41 +1,63 @@
-import React from "react"
-import {useState, useEffect} from "react"
-import {Link} from "react-router-dom"
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function Artist() {
-  const [artists, setArtists] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:4000/artists")
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data);
-        setArtists(data);
-      });
-  }, []);
+	const [artists, setArtists] = useState([]);
+	const [top5, setTop5] = useState([]);
 
-  return (
-    <div className="App">
-      <h1>Artists</h1>
-      {artists.map((artist) => {
-        return (
-          <div key={artist.artist} className="artistCard">
-            <Link to={`/artists/${artist._id}`} key={artist._id}>
-              <img src={artist.photo} alt={`${artist.artist}`} />
-              <h3>{artist.artist}</h3>
-              <div className="microphoneIcon">
-                <img
-                  src="https://img.icons8.com/emoji/48/000000/microphone-emoji.png"
-                  alt="microphone to like"
-                />
-                <p>{artist.likes.length}</p>
-                <p>{artist._id}</p>
-              </div>
-            </Link>
-          </div>
-        );
-      })}
-    </div>
-  );
+	useEffect(() => {
+		fetch('http://localhost:4000/artists')
+			.then((res) => res.json())
+			.then((data) => {
+				//Set artists and set the top 5 artists
+				let top = [];
+
+				data.sort((a, b) => {
+					return b.likes.length - a.likes.length;
+				});
+
+				for (let x = 0; x < 5; x++) {
+					top.push(data[x]);
+				}
+				setTop5(top);
+				let randomArtists = [];
+				randomArtists.length = data.length;
+				data.forEach((artist) => {
+					randomArtists.splice(
+						Math.floor(Math.floor(Math.random() * data.length)),
+						0,
+						artist
+					);
+				});
+				setArtists(randomArtists);
+				console.log(artists);
+			});
+	}, []);
+
+	return (
+		<div className='App'>
+			<h1>Artists</h1>
+			{artists.map((artist) => {
+				return (
+					<div key={artist.artist} className='artistCard'>
+						<Link to={`/artists/${artist._id}`} key={artist._id}>
+							<img src={artist.photo} alt={`${artist.artist}`} />
+							<h3>{artist.artist}</h3>
+							<div className='microphoneIcon'>
+								<img
+									src='https://img.icons8.com/emoji/48/000000/microphone-emoji.png'
+									alt='microphone to like'
+								/>
+								<p>{artist.likes.length}</p>
+								<p>{artist._id}</p>
+							</div>
+						</Link>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
-export default Artist
+export default Artist;
